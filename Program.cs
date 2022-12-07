ead8e2b28acb2f2c06bdc7093e6e560da613b113
Leisan <kapatyn@mail.ru>
2022-12-07 21:17:19 +0500

    изменения

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
-int a,b;
+int a, b;
 Console.Write($"Введите первое число: ");
 int.TryParse(Console.ReadLine()!, out a);
 Console.Write($"Введите второе число: ");
@@ -14,7 +14,7 @@ if (a > b)
 {
     Console.Write($" {a} ");
 }
-else 
+else
 {
     Console.Write($"{b}");
 }
